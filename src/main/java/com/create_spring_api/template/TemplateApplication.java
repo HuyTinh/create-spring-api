27e8ad316c30355f4547fa09bb529195b3facc52
HuyTinh <tinhnth15112003@gmail.com>
2025-02-22 21:52:16 +0700

    first commit

---
+++ src/main/java/com/create_spring_api/template/TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.create_spring_api.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/create_spring_api/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.create_spring_api.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
